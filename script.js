2da8b5cb2c16aa657beb4a7f5a2da5611190074a
saleh <salehalakhras2000@gmail.com>
2022-03-02 17:03:15 +0200

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//a function that randomly returns either rock,paper or scissor
 function computerPlay() {
     const computerChoice = Math.floor(Math.random() * 3) + 1;
 
@@ -9,26 +10,63 @@ function computerPlay() {
         return 'scissor';
 }
 
+
 function playRound(computerSelection, playerSelection) {
     if (computerSelection === playerSelection.toLowerCase())
         gameResult.textContent = 'Tie! ';
-    else if (computerSelection === 'rock' && playerSelection.toLowerCase() === 'paper')
+
+    else if (computerSelection === 'rock' && playerSelection.toLowerCase() === 'paper') {
         gameResult.textContent = 'You Win! Paper beats Rock.';
-    else if (computerSelection === 'rock' && playerSelection.toLowerCase() === 'scissor')
+        playerScore++;
+    } else if (computerSelection === 'rock' && playerSelection.toLowerCase() === 'scissor') {
         gameResult.textContent = 'You Lose! Rock beats Scissor.';
-    else if (computerSelection === 'paper' && playerSelection.toLowerCase() === 'rock')
+        computerScore++;
+    } else if (computerSelection === 'paper' && playerSelection.toLowerCase() === 'rock') {
         gameResult.textContent = 'You Lose! Paper beats Rock.';
-    else if (computerSelection === 'paper' && playerSelection.toLowerCase() === 'scissor')
+        computerScore++;
+    } else if (computerSelection === 'paper' && playerSelection.toLowerCase() === 'scissor') {
         gameResult.textContent = 'You Win! Scissor beats Paper.';
-    else if (computerSelection === 'scissor' && playerSelection.toLowerCase() === 'paper')
+        playerScore++;
+    } else if (computerSelection === 'scissor' && playerSelection.toLowerCase() === 'paper') {
         gameResult.textContent = 'You Lose! Scissor beats Paper.';
-    else if (computerSelection === 'scissor' && playerSelection.toLowerCase() === 'rock')
+        computerScore++;
+    } else if (computerSelection === 'scissor' && playerSelection.toLowerCase() === 'rock') {
         gameResult.textContent = 'You Win! Rock beats Scissor.';
+        playerScore++;
+    }
 }
+
+function clearScore() {
+    playerScore = 0;
+    computerScore = 0;
+}
+
+function checkGameScore() {
+    if (playerScore === 5) {
+        winner.textContent = 'Congratulations!, You Won.';
+        txtContainer.insertBefore(winner, txtContainer.firstChild);
+        clearScore();
+    } else if (computerScore === 5) {
+        winner.textContent = 'Sorry, The Computer Won.';
+        txtContainer.insertBefore(winner, txtContainer.firstChild);
+        clearScore();
+    }
+
+}
+
+
 const gameResult = document.createElement('div');
 const gameScore = document.createElement('div');
-const button = document.querySelectorAll('button');
-button.forEach(btn => btn.addEventListener('click', function(e) {
+const button = document.querySelectorAll('button:not(#reset)');
+const txtContainer = document.querySelector('.txtContainer');
+const winner = document.createElement('div');
+winner.classList.add('winner');
+var playerScore = 0;
+var computerScore = 0;
+button.forEach(btn => btn.addEventListener('click', (e) => {
     playRound(computerPlay(), e.target.id);
-    btn.parentNode.appendChild(gameResult);
+    txtContainer.appendChild(gameResult);
+    gameScore.textContent = `Player: ${playerScore} , Computer: ${computerScore}`;
+    txtContainer.appendChild(gameScore);
+    checkGameScore();
 }))
